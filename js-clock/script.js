b515c4a964ddf38b4aad37582737c3305f43da5f
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-08-11 21:40:37 +0300

    feat: implement the clock face using svg

--- js-clock/script.js
+++ js-clock/script.js
@@ -1,13 +1,19 @@
-setInterval(() => {
-	const now = new Date();
+for (let i = 1; i < 61 ; i++) {
+	dashes.innerHTML += `<line 
+                            x1="150" y1="0" 
+                            x2="150" y2="6" 
+                            stroke="black" stroke-width="${i % 5 ? 1 : 4}"
+                            transform="rotate(${i * 6} 150 150)" />`;
+}
+
+setTimeout(function run() {
+  const now = new Date();
 	const move = (hand, angle) => hand.setAttribute('transform', `rotate(${angle} 150 150)`);
-  const seconds = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
-  const minutes = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
-  const weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'].filter((i, idx) => idx === now.getDay());
-  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'].filter((i, idx) => idx === now.getMonth());
-  date.textContent = weekDay + ', ' + now.getDate() + ' ' + month + ' ' + now.getFullYear();
-  time.textContent = now.getHours() + ':' + minutes + ':' + seconds;
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  date.textContent = now.toLocaleDateString('en-US', options);
+  time.textContent = now.toLocaleTimeString('en-GB');
 	move(sec, 6 * now.getSeconds());
 	move(min, 6 * now.getMinutes() + now.getSeconds() / 10);
 	move(hour, 30 * (now.getHours() % 12) + now.getMinutes() / 2);
+  setTimeout(run, 1000);
 }, 1000);
