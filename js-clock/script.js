e15662db993a65b7cacac26e1b7a1631940acd46
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-08-12 23:19:14 +0300

    feat: add pie charts of time and clock brand

--- js-clock/script.js
+++ js-clock/script.js
@@ -15,5 +15,19 @@ setTimeout(function run() {
 	move(sec, 6 * now.getSeconds());
 	move(min, 6 * now.getMinutes() + now.getSeconds() / 10);
 	move(hour, 30 * (now.getHours() % 12) + now.getMinutes() / 2);
+  const ringParam = (elem, time) => {
+    time = (time === 0) ? 60 : time;
+    const offset = 5 * time / 3;
+    elem.setAttribute('stroke-dasharray', `${offset} ${100 - offset}`);
+  };
+  ringParam(ringMin, now.getMinutes());
+  ringParam(ringSec, now.getSeconds());
+  let hours = (now.getHours() > 12) ? now.getHours() - 12 : now.getHours();
+  hours = (hours % 12) ? hours : 12;
+  const hourOffs = 25 * hours / 3;
+	ringHour.setAttribute('stroke-dasharray', `${hourOffs} ${100 - hourOffs}`);
+  hText.textContent = (now.getHours() < 10) ? '0' + now.getHours() : now.getHours();
+  mText.textContent = (now.getMinutes() < 10) ? '0' + now.getMinutes() : now.getMinutes();
+  sText.textContent = (now.getSeconds() < 10) ? '0' + now.getSeconds() : now.getSeconds();
   setTimeout(run, 1000);
 }, 1000);
