b0a32e281c645e7a101cc7be1d97c33c10981f33
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-08-22 22:08:57 +0300

    feat: add overlay for video controls events

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -3,10 +3,14 @@ const muteAudio = () => {
   video.muted ? video.muted = false : video.muted = true;
 };
 
-const handleVolSlider = () => vol.value = video.volume * 100;
-
 const progressBarApperance = (value) =>`
   linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+
+const overlayApperance = (overlayInner) => {
+  overlay.innerHTML = overlayInner;
+  overlay.classList.remove('hidden');
+  setTimeout(() => overlay.classList.add('hidden'), 1000);
+};
 
 document.addEventListener('click', (e) => {
   switch (e.target.id) {
@@ -16,7 +20,7 @@ document.addEventListener('click', (e) => {
       playBtnBig.classList.toggle('hidden');
       video.paused ? video.play() : video.pause();
       break;
-    case 'muteBtn': muteAudio(); break;
+    case 'muteBtn': return muteAudio();
     case 'fullscreenBtn': return video.requestFullscreen();
     default: break;
   };
@@ -24,40 +28,64 @@ document.addEventListener('click', (e) => {
 
 document.addEventListener('keyup', (e) => {
   switch (e.code) {
-    case 'KeyK':
-    case 'Space': return video.paused ? video.play() : video.pause();
-    case 'KeyM': muteAudio(); break;
-    case 'Period':
-      if (video.paused) video.currentTime += 0.17;
+    case 'KeyK': return video.paused ? video.play() : video.pause();
+    case 'KeyM': 
+      muteAudio();
       break;
-    case 'Comma': 
-      if (video.paused) video.currentTime -= 0.17;
-      break;
-    case 'ArrowUp': return video.volume > 0.95 ? video.volume = 1 : video.volume += 0.05;
-    case 'ArrowDown': return video.volume < 0.05 ? video.volume = 0 : video.volume -= 0.05;
-    case 'ArrowLeft': return video.currentTime > 5 ? 
-                                                    video.currentTime -= 5 : 
-                                                    video.currentTime = 0;
-    case 'ArrowRight': return video.currentTime < video.duration - 5 ? 
-                                                                      video.currentTime += 5 : 
-                                                                      video.currentTime = video.duration;
-    case 'KeyF': 
-      document.fullscreenElement ? document.exitFullscreen() : video.requestFullscreen();
-    case 'KeyJ': return video.currentTime > 10 ? 
-                                                video.currentTime -= 10 : 
-                                                video.currentTime = 0;
-    case 'KeyL': return video.currentTime < video.duration - 10 ? 
-                                                                video.currentTime += 10 : 
-                                                                video.currentTime = video.duration;
+    case 'KeyF': return document.fullscreenElement ? document.exitFullscreen() : video.requestFullscreen();
     default: break;
   };
-  if (!isNaN(+e.key)) video.currentTime = +e.key * video.duration / 10; 
-  if (e.shiftKey && e.code === 'Period') video.playbackRate === 2 ? 
-                                                                  video.playbackRate = 2 : 
-                                                                  video.playbackRate += 0.25;
-  if (e.shiftKey && e.code === 'Comma') video.playbackRate === 0.25 ? 
-                                                                    video.playbackRate = 0.25 : 
-                                                                    video.playbackRate -= 0.25;
+  if (!isNaN(+e.key) && +e.key > 0) video.currentTime = +e.key * video.duration / 10; 
+  if (e.shiftKey && e.code === 'Period') {
+    video.playbackRate === 2 ? video.playbackRate = 2 : video.playbackRate += 0.25;
+    overlayApperance(`${video.playbackRate}x`);
+  };
+  if (e.shiftKey && e.code === 'Comma') {
+    video.playbackRate === 0.25 ? video.playbackRate = 0.25 : video.playbackRate -= 0.25;
+    overlayApperance(`${video.playbackRate}x`);
+  };
+});
+
+document.addEventListener('keydown', (e) => {
+  switch (e.code) {
+    case 'ArrowUp':
+      if (!video.muted) video.volume > 0.95 ? video.volume = 1 : video.volume += 0.05;
+      else {
+        video.muted = false;
+        video.volume = 0.05;
+      };
+      break;
+    case 'ArrowDown':
+      if (!video.muted) video.volume < 0.05 ? video.volume = 0 : video.volume -= 0.05;
+      break;
+    case 'ArrowLeft':
+      video.currentTime > 5 ? video.currentTime -= 5 : video.currentTime = 0;
+      overlayApperance('⏪ 5 sec');
+      break;
+    case 'ArrowRight':
+      video.currentTime < video.duration - 5 ? video.currentTime += 5 : video.currentTime = video.duration;
+      overlayApperance('⏩ 5 sec');
+      break;
+    case 'KeyJ':
+      video.currentTime > 10 ? video.currentTime -= 10 : video.currentTime = 0;
+      overlayApperance('⏪ 10 sec');
+      break;
+    case 'KeyL':
+      video.currentTime < video.duration - 10 ? video.currentTime += 10 : video.currentTime = video.duration;
+      overlayApperance('⏩ 10 sec');
+      break;
+    case 'Period':
+      if (video.paused) {
+        video.currentTime += 0.17;
+        overlayApperance('⏩');
+      }; break;
+    case 'Comma': 
+      if (video.paused) {
+        video.currentTime -= 0.17;
+        overlayApperance('⏪');
+      }; break;
+    default: break;
+  };
 });
 
 video.addEventListener('play', () => {
@@ -80,18 +108,36 @@ progress.addEventListener('input', () => {
 });
 
 vol.addEventListener('input', () => {
-  video.volume = vol.value / 100;
+  if (!video.muted) video.volume = vol.value / 100;
+  else {
+    video.muted = false;
+    video.volume = 0.05;
+  };
 });
 
-document.addEventListener('wheel', (e) => {
+player.addEventListener('wheel', (e) => {
   let volume = video.volume * 100;
-  e.deltaY < 0 ? volume += 5 : volume -= 5;
+  if (e.deltaY < 0 && !video.muted) volume += 5;
+  else if (e.deltaY < 0 && video.muted) {
+    video.muted = false;
+    volume = 5;
+  } else if (e.deltaY > 0 && !video.muted) volume -= 5;
   if (volume < 0) video.volume = 0;
   else if (volume > 100) video.volume = 1;
   else video.volume = volume / 100;
 });
 
 video.addEventListener('volumechange', () => {
-  vol.style.background = progressBarApperance(video.volume * 100);
-  handleVolSlider();
+  if (video.muted) {
+    overlayApperance('muted');
+    vol.style.background = progressBarApperance(0);
+    vol.value = 0;
+  } else {
+    overlayApperance(`🔊 ${Math.floor(video.volume * 100)}%`);
+    vol.style.background = progressBarApperance(video.volume * 100);
+    vol.value = video.volume * 100;
+    if (video.volume < 0.5 && !muteBtn.classList.contains('vol50')) muteBtn.classList.add('vol50');
+    else if (video.volume >= 0.5 && muteBtn.classList.contains('vol50')) muteBtn.classList.remove('vol50');
+  };
+  video.volume === 0 || video.muted ? muteBtn.classList.add('muted') : muteBtn.classList.remove('muted');
 });
